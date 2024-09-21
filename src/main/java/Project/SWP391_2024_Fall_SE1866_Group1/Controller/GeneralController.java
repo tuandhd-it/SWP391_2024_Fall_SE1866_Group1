cedd10999a7eff2762ec8243622258912a36f9a4
DHDT <tuandhdhe181591@fpt.edu.vn>
2024-09-22 03:24:00 +0700

    Create file GeneralController

---
+++ src/main/java/Project/SWP391_2024_Fall_SE1866_Group1/Controller/GeneralController.java
@@ -0,0 +1,39 @@
+package Project.SWP391_2024_Fall_SE1866_Group1.Controller;
+
+import Project.SWP391_2024_Fall_SE1866_Group1.Service.ReceptionistService;
+import Project.SWP391_2024_Fall_SE1866_Group1.dto.request.ReceptionistCreationRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Controller
+public class GeneralController {
+
+    @Autowired
+    private ReceptionistService receptionistService;
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String loginPost(Model model) {
+
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerPost(@RequestBody ReceptionistCreationRequest request) {
+        receptionistService.createReceptionist(request);
+        return "landing_page";
+    }
+}
