5efae5e364bd22ac1162bbddf9051104f46b22d6
LamSB293 <lamnvhe181525@fpt.edu.vn>
2024-09-25 19:28:22 +0700

    Admin Controller

--- src/main/java/Project/SWP391_2024_Fall_SE1866_Group1/Controller/AdminController.java
+++ src/main/java/Project/SWP391_2024_Fall_SE1866_Group1/Controller/AdminController.java
@@ -1,9 +1,34 @@
 package Project.SWP391_2024_Fall_SE1866_Group1.Controller;
 
+import Project.SWP391_2024_Fall_SE1866_Group1.Entity.Branch;
+import Project.SWP391_2024_Fall_SE1866_Group1.Service.AdminService;
+import Project.SWP391_2024_Fall_SE1866_Group1.dto.request.ClinicBranchRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+
+    @Autowired
+    private AdminService adminService;
+
+    @PostMapping("/branch")
+    Branch createBranch(@RequestBody ClinicBranchRequest branchRequest) {
+        return adminService.createBranch(branchRequest);
+    }
+
+    @GetMapping
+    List<Branch> getAllBranches() {
+        return adminService.getAllBranches();
+    }
+
+
+
 }
