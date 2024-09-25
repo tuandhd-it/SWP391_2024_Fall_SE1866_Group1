4512691a31afaa2f2f838845304346bba60b370f
LamSB293 <lamnvhe181525@fpt.edu.vn>
2024-09-25 19:27:50 +0700

    CU Clinic

---
+++ src/main/java/Project/SWP391_2024_Fall_SE1866_Group1/dto/request/ClinicBranchRequest.java
@@ -0,0 +1,18 @@
+package Project.SWP391_2024_Fall_SE1866_Group1.dto.request;
+
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ClinicBranchRequest {
+    String branchName;
+    String branch_description;
+    String branch_address;
+    String branch_phone;
+    String branch_img;
+}
