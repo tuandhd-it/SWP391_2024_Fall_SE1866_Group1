2e0feb2ecb4e28ea445f79eb8a4a7059acd3c51c
KzDiary <khanhnthe182090@fpt.edu.vn>
2024-11-06 23:42:00 +0700

    Fix RecordRepository

--- src/main/java/project/dental_clinic_management/repository/RecordRepository.java
+++ src/main/java/project/dental_clinic_management/repository/RecordRepository.java
@@ -6,11 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import project.dental_clinic_management.entity.Record;
 
+import java.util.List;
+
 
 @Repository
 public interface RecordRepository extends JpaRepository<Record, Integer> {
     Page<Record> getRecordByPatient_PatientId(int patientId, Pageable pageable);
-
+    List<Record> getRecordByPatient_PatientId(int patientId);
     Record findByPatientId(int patientId);
 
     Record findByRecordId(long recordId);
